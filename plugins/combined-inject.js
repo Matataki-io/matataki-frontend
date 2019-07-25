d787fe7fbc191849238ed17ea163772a948738dc
zxplus <shellteo@163.com>
2019-07-25 10:40:31 +0800

    update axios

--- plugins/combined-inject.js
+++ plugins/combined-inject.js
@@ -1,7 +1,9 @@
 import API from '@/api/API'
 import backendAPI from '@/api/backend'
+import endpoint from '@/api/endpoint'
 
 export default ({ app }, inject) => {
   inject('API', API)
   inject('backendAPI', backendAPI)
+  inject('endpoint', endpoint)
 }
