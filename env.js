1f950a5357cd7d0cbcbd6b0a64d085b2fa346ea8
zxplus <shellteo@163.com>
2019-07-25 16:57:28 +0800

    update article page

--- env.js
+++ env.js
@@ -5,6 +5,7 @@ module.exports = {
     VUE_APP_SIGNATURE_CONTRACT: 'dragonquest2',
     VUE_APP_SCRIPT_HASH: '36df9722fc0ff5fa3979f2a844a012cabe1d4c56',
     VUE_APP_GITHUB_CLIENT_ID: '889e6eafa77e2e87a08c',
+    ssImgAddress: 'https://ssimg.frontenduse.top'
   },
   production: {
     VUE_APP_API: 'https://api.smartsignature.io',
@@ -12,5 +13,6 @@ module.exports = {
     VUE_APP_SIGNATURE_CONTRACT: 'signature.bp',
     VUE_APP_SCRIPT_HASH: '36df9722fc0ff5fa3979f2a844a012cabe1d4c56',
     VUE_APP_GITHUB_CLIENT_ID: '21cc5341ef03f40cd5a4',
+    ssImgAddress: 'https://ssimg.frontenduse.top'
   }
 }
