fadadd87117c45469926fb0731a0254016643a97
zxplus <shellteo@163.com>
2019-08-01 20:25:16 +0800

    update env

--- env.js
+++ env.js
@@ -6,7 +6,7 @@ module.exports = {
     VUE_APP_SCRIPT_HASH: '36df9722fc0ff5fa3979f2a844a012cabe1d4c56',
     VUE_APP_GITHUB_CLIENT_ID: '889e6eafa77e2e87a08c',
     ssImgAddress: 'https://ssimg.frontenduse.top',
-    VUE_PC_URL: 'https://testpc.smartsignature.io'
+    VUE_APP_PC_URL: 'https://wwwtest.smartsignature.io'
   },
   production: {
     VUE_APP_API: 'https://api.smartsignature.io',
@@ -15,6 +15,6 @@ module.exports = {
     VUE_APP_SCRIPT_HASH: '36df9722fc0ff5fa3979f2a844a012cabe1d4c56',
     VUE_APP_GITHUB_CLIENT_ID: '21cc5341ef03f40cd5a4',
     ssImgAddress: 'https://ssimg.frontenduse.top',
-    VUE_PC_URL: 'https://pc.smartsignature.io'
+    VUE_APP_PC_URL: 'https://www.smartsignature.io'
   }
 }
