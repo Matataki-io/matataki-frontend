2c462390cf75cb09b1f1721509d8c14e914e3772
xiaotiandada <952822399@qq.com>
2019-08-01 17:11:12 +0800

    error page
    error page

---
+++ layouts/error.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="error">
+    <h2 v-if="error.statusCode==404">
+      ⚠️ 404页面不存在
+    </h2>
+    <h2 v-else>
+      ❗ 500服务器错误
+    </h2>
+    <nuxt-link class="link" to="/">
+      回到主页
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['error']
+}
+</script>
+
+<style lang="less" scoped>
+.error {
+  min-height: calc(100% - 189px);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+h2 {
+  font-size: 40px;
+  text-align: center;
+  margin: 0;
+  padding: 0;
+}
+.link {
+  display: block;
+  text-align: center;
+  color: #333;
+  font-size: 18px;
+  padding: 0;
+  margin: 30px 0 0;
+}
+</style>
