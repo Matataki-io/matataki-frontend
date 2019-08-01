5a56a16d20ef4219fc55da93570e3f709a82a09a
xiaotiandada <952822399@qq.com>
2019-08-01 20:48:38 +0800

    跳转
    跳转

--- layouts/default.vue
+++ layouts/default.vue
@@ -43,22 +43,24 @@ export default {
 
   },
   mounted() {
-    const isPC = () => {
-      const userAgentInfo = navigator.userAgent
-      const Agents = ['Android', 'iPhone',
-        'SymbianOS', 'Windows Phone',
-        'iPad', 'iPod']
-      let flag = true
-      for (let v = 0; v < Agents.length; v++) {
-        if (userAgentInfo.indexOf(Agents[v]) > 0) {
-          flag = false
-          break
+    (function () {
+      const isPC = () => {
+        const userAgentInfo = navigator.userAgent
+        const Agents = ['Android', 'iPhone',
+          'SymbianOS', 'Windows Phone',
+          'iPad', 'iPod']
+        let flag = true
+        for (let v = 0; v < Agents.length; v++) {
+          if (userAgentInfo.indexOf(Agents[v]) > 0) {
+            flag = false
+            break
+          }
         }
+        return flag
       }
-      return flag
-    }
 
-    if (!isPC()) window.location.href = process.env.VUE_APP_URL
+      if (!isPC()) window.location.href = process.env.VUE_APP_URL
+    })()
 
     this.$store.dispatch('testLogin')
   },
