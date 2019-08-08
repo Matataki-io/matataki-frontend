c2f5507adb2724528e3c8f38b69be39b4e58a5bd
xiaotiandada <952822399@qq.com>
2019-08-08 19:24:21 +0800

    footer link
    footer link

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,7 +1,6 @@
 <template>
   <footer class="footer">
     <div class="mw footer-content">
-      <img class="logo" src="@/assets/img/footer_logo.png" alt="logo">
       <div class="content">
         <div class="nav">
           <a class="href" target="_blank" href="http://www.smartsignature.io/p/617">使用手册</a>
@@ -9,6 +8,17 @@
           <a class="href" href="javascript:;">用户协议</a>
           <a class="href" target="_blank" href="https://github.com/smart-signature/smart-signature-future">关于我们</a>
           <a class="href" target="_blank" href="http://www.smartsignature.io/p/616">智能公告牌</a>
+        </div>
+        <p class="link">
+          友情链接:
+          <a href="http://linkidol.pro/" target="_blank">链接偶像</a>
+          |
+          <a href="https://orange.xyz/" target="_blank">橙皮书</a>
+        </p>
+      </div>
+      <div>
+        <div>
+          <img class="logo" src="@/assets/img/footer_logo.png" alt="logo">
           <div class="icon wechat-icon">
             <div class="wechat">
               <div class="wechat-img">
@@ -53,7 +63,7 @@ export default {}
   &-content {
     height: 100%;
     display: flex;
-    align-items: flex-start;
+    align-items: center;
     justify-content: space-between;
     box-sizing: border-box;
   }
@@ -69,7 +79,7 @@ export default {}
     display: flex;
     align-items: center;
     .href {
-      margin-left: 20px;
+      margin-right: 20px;
       text-decoration: none;
       font-size:14px;
       color:rgba(178,178,178,1);
@@ -78,87 +88,107 @@ export default {}
         color: #fff;
       }
     }
-    .icon {
-      margin-left: 40px;
-      display: inline-block;
-      cursor: pointer;
-      .wechat {
-        display: none;
-        justify-content: space-between;
-        align-items: center;
-        position: absolute;
-        top: -200px;
-        left: -146px;
-        width: 310px;
-        background: #fff;
-        border-radius: 6px;
-        padding: 14px;
-        box-sizing: border-box;
-        box-shadow: 0 0 10px rgba(0,0,0,.1);
-        &-img {
-          text-align: center;
-        }
-        &-title {
-          padding: 0;
-          margin: 0;
-          font-size: 14px;
-          line-height: 1.5;
-          color: #000;
-        }
-        &::after {
-          position: absolute;
-          bottom: -20px;
-          left: 50%;
-          transform: translate(-50%, 0);
-          display: block;
-          content: '';
-          width: 0;
-          height: 0;
-          border: 10px solid #fff;
-          border-color: #fff transparent transparent;
-        }
-        img {
-          border: 1px solid #eee;
-          width: 130px;
-          box-sizing: border-box;
-        }
+
+  }
+
+}
+
+.icon {
+  margin-left: 22px;
+  display: inline-block;
+  cursor: pointer;
+  .wechat {
+    display: none;
+    justify-content: space-between;
+    align-items: center;
+    position: absolute;
+    top: -200px;
+    left: -146px;
+    width: 310px;
+    background: #fff;
+    border-radius: 6px;
+    padding: 14px;
+    box-sizing: border-box;
+    box-shadow: 0 0 10px rgba(0,0,0,.1);
+    &-img {
+      text-align: center;
+    }
+    &-title {
+      padding: 0;
+      margin: 0;
+      font-size: 14px;
+      line-height: 1.5;
+      color: #000;
+    }
+    &::after {
+      position: absolute;
+      bottom: -20px;
+      left: 50%;
+      transform: translate(-50%, 0);
+      display: block;
+      content: '';
+      width: 0;
+      height: 0;
+      border: 10px solid #fff;
+      border-color: #fff transparent transparent;
+    }
+    img {
+      border: 1px solid #eee;
+      width: 130px;
+      box-sizing: border-box;
+    }
+  }
+  &:hover .wechat {
+    display: flex;
+  }
+  &.wechat-icon {
+    width: 28px;
+    height: 23px;
+    position: relative;
+    background-image: url('../../assets/img/footer_wechat.svg');
+    &:hover {
+      background-image: url('../../assets/img/footer_wechat_active.svg');
+    }
+  }
+  &.github-icon {
+    width: 22px;
+    height: 24px;
+    background-image: url('../../assets/img/footer_github.svg');
+    &:hover {
+      background-image: url('../../assets/img/footer_github_active.svg');
+    }
+  }
+  &.telegram-icon{
+    width: 25px;
+    height: 21px;
+    background-image: url('../../assets/img/footer_telegram.svg');
+    &:hover {
+      background-image: url('../../assets/img/footer_telegram_active.svg');
+    }
+  }
+}
+.copyright {
+  font-size: 14px;
+  color: rgba(178, 178, 178, 1);
+  line-height: 22px;
+  margin: 10px 0 0;
+}
+
+  .link {
+    margin: 10px 0 0;
+    font-size:14px;
+    color:rgba(178,178,178,1);
+    line-height:22px;
+    a {
+      color:rgba(178,178,178,1);
+      text-decoration: none;
+      margin: 0 4px;
+      &:nth-child(1) {
+        margin-left: 12px;
       }
-      &:hover .wechat {
-        display: flex;
-      }
-      &.wechat-icon {
-        width: 28px;
-        height: 23px;
-        position: relative;
-        background-image: url('../../assets/img/footer_wechat.svg');
-        &:hover {
-          background-image: url('../../assets/img/footer_wechat_active.svg');
-        }
-      }
-      &.github-icon {
-        width: 22px;
-        height: 24px;
-        background-image: url('../../assets/img/footer_github.svg');
-        &:hover {
-          background-image: url('../../assets/img/footer_github_active.svg');
-        }
-      }
-      &.telegram-icon{
-        width: 25px;
-        height: 21px;
-        background-image: url('../../assets/img/footer_telegram.svg');
-        &:hover {
-          background-image: url('../../assets/img/footer_telegram_active.svg');
-        }
+      &:hover {
+        color: #fff;
       }
     }
   }
-  .copyright {
-    font-size: 14px;
-    color: rgba(178, 178, 178, 1);
-    line-height: 22px;
-    text-align: right;
-    margin: 36px 0 0;
-  }
-}
 </style>
