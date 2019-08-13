24af09eabfa0223720a778f7e0cb7fb1c1aad22c
xiaotiandada <952822399@qq.com>
2019-08-13 15:36:26 +0800

    覆盖默认的meta
    覆盖默认的meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,9 +26,10 @@ export default {
     // title: process.env.npm_package_name || '',
     title: '智能签名SmartSignature | 用技术促进传播',
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       // { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { hid: 'og:title', name: 'og:title', content: '智能签名SmartSignature | 用技术促进传播' },
+      { hid: 'og:site_name', name: 'og:site_name', content: '智能签名SmartSignature | 用技术促进传播' },
+      { hid: 'og:description', name: 'og:description', content: 'Sign it if you mean it! A blockchain  Dapp 投资好文，分享有收益！' },
       { hid: 'description', name: 'description', content: '智能签名SmartSignature - 由仙女座科技公司开发的促进优质内容裂变传播的工具，凭借经验丰富的区块链应用和智能合约设计开发能力，为用户提供旨在为创作者和分享者创造价值，为接收者发现价值。' },
       { hid: 'keywords', name: 'keywords', content: '岛娘,小岛美奈子,唐飞虎,仙女座科技,智能签名,智能公告牌,智能投资,裂变营销,价值投资,区块链,比特币,以太坊,去中心化社交,去中心化商店,去中心化,DApp,EOS' }
     ],
