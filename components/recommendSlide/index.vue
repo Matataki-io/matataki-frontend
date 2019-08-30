b889c209d098df2aeb8db0be2213962beccf2fdb
xiaotiandada <952822399@qq.com>
2019-08-30 19:57:36 +0800

    推荐加标题

--- components/recommendSlide/index.vue
+++ components/recommendSlide/index.vue
@@ -1,10 +1,15 @@
 <template>
-  <n-link target="_blank" :to="{name: 'p-id', params: {id: card.id}}" class="slide">
-    <img
-      v-if="cover"
-      :src="cover"
-      alt="cover"
-    >
+  <n-link target="_blank" :to="{name: 'p-id', params: {id: card.id}}">
+    <div class="slide">
+      <img
+        v-if="cover"
+        :src="cover"
+        alt="cover"
+      >
+      <div class="title">
+        {{ card&& card.title }}
+      </div>
+    </div>
   </n-link>
 </template>
 
@@ -27,8 +32,6 @@ export default {
 <style lang="less" scoped>
 .slide {
   display: flex;
-  // max-width: 787px;
-  // flex: 0 0 787px;
   height: 394px;
   background: #fff;
   box-sizing: border-box;
@@ -38,6 +41,7 @@ export default {
   cursor: pointer;
   transition: all 0.3s;
   transform: rotate(0deg);
+  position: relative;
   &:hover {
     box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.1);
   }
@@ -49,6 +53,19 @@ export default {
     height: 100%;
     object-fit: cover;
     transition: all 0.3s;
+  }
+
+  .title {
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.7);
+    color: #fff;
+    font-size: 22px;
+    line-height: 1.5;
+    padding: 14px 20px;
+    font-weight: bold;
   }
 }
 
