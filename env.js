974b674d96994b46a64d7ad240868d9023f84bb3
xiaotiandada <952822399@qq.com>
2019-08-31 16:20:10 +0800

    修改链接

--- env.js
+++ env.js
@@ -11,12 +11,12 @@ module.exports = {
   },
   production: {
     VUE_APP_API: 'https://api.smartsignature.io',
-    VUE_APP_URL: 'https://smartsignature.io',
+    VUE_APP_URL: 'https://matataki.io',
     VUE_APP_SIGNATURE_CONTRACT: 'signature.bp',
     VUE_APP_SCRIPT_HASH: '36df9722fc0ff5fa3979f2a844a012cabe1d4c56',
     VUE_APP_GITHUB_CLIENT_ID: '21cc5341ef03f40cd5a4',
     ssImgAddress: 'https://ssimg.frontenduse.top',
-    VUE_APP_PC_URL: 'https://www.smartsignature.io',
+    VUE_APP_PC_URL: 'https://www.matataki.io',
     WX_SHARE_HOST: 'https://smartsignature.frontenduse.top'
   }
 }
