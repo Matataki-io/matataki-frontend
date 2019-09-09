e4d25691072a73475f0fb13eb499e1f9a5f744c5
xiaotiandada <952822399@qq.com>
2019-09-09 15:18:02 +0800

    update meta

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,6 +26,9 @@ export default {
     // title: process.env.npm_package_name || '',
     title: '瞬MATATAKI',
     meta: [
+      { hid: 'robots', name: 'robots', property: 'robots', content: 'index,follow' },
+      { hid: 'copyright', name: 'copyright', property: 'copyright', content: 'Copyright © 2018-2019 ANDOROMEDA TECH.ltd' },
+      { hid: 'description', name: 'description', property: 'description', content: '瞬MATATAKI是一个永久存储和版权确权的内容平台，通过IPFS协议保障内容的永久可访问和确权信息可查询。使用了通证经济来激励创作者、探索者、布道者共建未来的超级知识链接网络。' },
       { hid: 'keywords', name: 'keywords', content: '岛娘,小岛美奈子,唐飞虎,仙女座科技,瞬MATATAKI,智能公告牌,智能投资,裂变营销,价值投资,区块链,比特币,以太坊,去中心化社交,去中心化商店,去中心化,DApp,EOS' },
       /* <!--  Meta for Twitter Card --> */
       { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
@@ -39,8 +42,7 @@ export default {
       { hid: 'og:title', name: 'og:title', property: 'og:title', content: '瞬MATATAKI—发布瞬间，灵感永存' },
       { hid: 'og:image', name: 'og:image', property: 'og:image', content: 'https://ssimg.frontenduse.top/avatar/2019/08/30/c1d6ae7ed4e6102cb45d0a8f656d5569.png' },
       { hid: 'og:description', name: 'og:description', property: 'og:description', content: '瞬MATATAKI是一个永久存储和版权确权的内容平台，通过IPFS协议保障内容的永久可访问和确权信息可查询。使用了通证经济来激励创作者、探索者、布道者共建未来的超级知识链接网络。' },
-      { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article' },
-      { hid: 'description', name: 'description', property: 'description', content: '瞬MATATAKI是一个永久存储和版权确权的内容平台，通过IPFS协议保障内容的永久可访问和确权信息可查询。使用了通证经济来激励创作者、探索者、布道者共建未来的超级知识链接网络。' }
+      { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article' }
       /* end */
     ],
     link: [
--- pages/p/_id.vue
+++ pages/p/_id.vue
@@ -280,6 +280,7 @@ export default {
     return {
       title: this.article.title,
       meta: [
+        { hid: 'description', name: 'description', property: 'description', content: this.article.short_content },
         /* <!--  Meta for Twitter Card --> */
         { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
         { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '@Andoromeda' },
@@ -293,8 +294,7 @@ export default {
         { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article' },
         { hid: 'og:url', name: 'og:url', property: 'og:url', content: `${process.env.VUE_APP_PC_URL}/p/${this.article.id}` },
         { hid: 'og:image', name: 'og:image', property: 'og:image', content: this.$API.getImg(this.article.cover) },
-        { hid: 'og:description', name: 'og:description', property: 'og:description', content: this.article.short_content },
-        { hid: 'description', name: 'description', property: 'description', content: this.article.short_content }
+        { hid: 'og:description', name: 'og:description', property: 'og:description', content: this.article.short_content }
         /* end */
       ]
     }
