521e498b9cd350e331791f2201749f19d3afa7ca
xiaotiandada <952822399@qq.com>
2019-09-10 21:14:50 +0800

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,14 +32,14 @@ export default {
       { hid: 'keywords', name: 'keywords', content: '岛娘,小岛美奈子,唐飞虎,仙女座科技,瞬MATATAKI,智能公告牌,智能投资,裂变营销,价值投资,区块链,比特币,以太坊,去中心化社交,去中心化商店,去中心化,DApp,EOS' },
       /* <!--  Meta for Twitter Card --> */
       { hid: 'twitter:card', name: 'twitter:card', property: 'twitter:card', content: 'summary' },
-      { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '瞬MATATAKI—发布瞬间，灵感永存' },
-      { hid: 'twitter:title', property: 'twitter:title', content: '瞬MATATAKI—发布瞬间，灵感永存' },
+      { hid: 'twitter:site', name: 'twitter:site', property: 'twitter:site', content: '瞬MATATAKI—发布瞬间，灵感永存1' },
+      { hid: 'twitter:title', property: 'twitter:title', content: '瞬MATATAKI—发布瞬间，灵感永存2' },
       { hid: 'twitter:image', name: 'twitter:image', property: 'twitter:image', content: 'https://ssimg.frontenduse.top/avatar/2019/08/30/c1d6ae7ed4e6102cb45d0a8f656d5569.png' },
       { hid: 'twitter:description', name: 'description', property: 'twitter:description', content: '瞬MATATAKI是一个永久存储和版权确权的内容平台，通过IPFS协议保障内容的永久可访问和确权信息可查询。使用了通证经济来激励创作者、探索者、布道者共建未来的超级知识链接网络。' },
       /* <!--  Meta for OpenGraph --> */
       { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'website' },
-      { hid: 'og:site_name', name: 'og:site_name', property: 'og:site_name', content: '瞬MATATAKI—发布瞬间，灵感永存' },
-      { hid: 'og:title', property: 'og:title', content: '瞬MATATAKI—发布瞬间，灵感永存' },
+      { hid: 'og:site_name', name: 'og:site_name', property: 'og:site_name', content: '瞬MATATAKI—发布瞬间，灵感永存3' },
+      { hid: 'og:title', property: 'og:title', content: '瞬MATATAKI—发布瞬间，灵感永存4' },
       { hid: 'og:image', name: 'og:image', property: 'og:image', content: 'https://ssimg.frontenduse.top/avatar/2019/08/30/c1d6ae7ed4e6102cb45d0a8f656d5569.png' },
       { hid: 'og:description', name: 'description', property: 'og:description', content: '瞬MATATAKI是一个永久存储和版权确权的内容平台，通过IPFS协议保障内容的永久可访问和确权信息可查询。使用了通证经济来激励创作者、探索者、布道者共建未来的超级知识链接网络。' },
       { hid: 'og:type', name: 'og:type', property: 'og:type', content: 'article' }
