39bbe20c0c3c421b0d3c4941f06c14b9b0d1b2b2
xiaotiandada <952822399@qq.com>
2019-10-14 11:04:27 +0800

    fix share code

--- components/modal/SocialShare.vue
+++ components/modal/SocialShare.vue
@@ -75,7 +75,7 @@ export default {
     shareLink() {
       const { protocol, host } = window.location
       // let url = `${process.env.WX_SHARE_HOST}/p/${this.$route.params.id}`
-      const url = `${protocol}//${host}/p/${this.$route.params.id}`
+      let url = `${protocol}//${host}/p/${this.$route.params.id}`
       if (this.isLogined) url += `?invite=${this.currentUserInfo.id}&referral=${this.currentUserInfo.id}`
       return url
     }
