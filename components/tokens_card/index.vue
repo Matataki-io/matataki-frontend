aec7201b0abb56590109da2d14df90a9463e49eb
xiaotiandada <952822399@qq.com>
2019-10-18 20:39:50 +0800

    调整文字

--- components/tokens_card/index.vue
+++ components/tokens_card/index.vue
@@ -96,7 +96,7 @@ export default {
       const typeList = {
         mint: '增发',
         transfer: '赠送',
-        exchange_purchase: '交易所内购买',
+        exchange_purchase: '交易',
         exchange_addliquidity: '交易所添加流动性',
         exchange_removeliquidity: '交易所删除流动性'
       }
