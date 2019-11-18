1578ae428bc83d2ff8543d6a56c9282e5a6a79b5
韦海晖 <franciswei98@gmail.com>
2019-11-18 12:19:04 +0800

    fix: referral 没有 URLSafe 就添加，可能导致分享出现问题

--- components/modal/SocialShare.vue
+++ components/modal/SocialShare.vue
@@ -69,9 +69,10 @@ export default {
     socialLink() {
       const title = this.article.title
       let link = encodeURIComponent(window.location.href)
-      if (this.isLogined) link += `/?referral=${this.currentUserInfo.id}`
+      if (this.isLogined) link += encodeURIComponent(`/?referral=${this.currentUserInfo.id}`)
       const pic = this.cover
-      const telegramIVLink = encodeURIComponent(`https://t.me/iv?url=${encodeURIComponent(link)}&rhash=6937cb5e3b86c1`)
+      // 用于 Telegram 的即时预览，模板ID为6937cb5e3b86c1，模板出问题了联系我 --Frank
+      const telegramIVLink = encodeURIComponent(`https://t.me/iv?url=${link}&rhash=6937cb5e3b86c1`)
       return {
         weibo: `http://service.weibo.com/share/share.php?appkey=&title=${title}&url=${link}&pic=${pic}&searchPic=false&style=simple`,
         // facebook: `https://www.facebook.com/sharer.php?title=${title}&href=${link}`,
