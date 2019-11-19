548432e7a2f9659056f09594bf27251bef811c57
xiaotian <952822399@qq.com>
2019-11-19 17:18:15 +0800

    个人主页购买页面

--- components/user/user_page_nav.vue
+++ components/user/user_page_nav.vue
@@ -36,7 +36,7 @@ export default {
         { title: this.$t('user.follow'), url: 'user-id-follow', self: false },
         { title: this.$t('user.buy'), url: 'user-id-buy', self: true },
         { title: this.$t('user.bookmark'), url: 'user-id-bookmark', self: true },
-        { title: this.$t('user.information'), url: 'user-id-information', self: false }
+        { title: this.$t('user.information'), url: 'user-id-info', self: false }
       ]
     }
   }
