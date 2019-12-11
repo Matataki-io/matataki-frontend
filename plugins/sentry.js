7d6ca0a7b3b65745bfee4c693fef89954c32da60
xiaotian <952822399@qq.com>
2019-12-11 14:43:12 +0800

    🔧 隐藏sentry

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -1,8 +1,8 @@
-import Vue from 'vue'
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations'
-// 账号在xiaotian这里 需要联系
-Sentry.init({
-  dsn: 'https://f741fd4ca0d74d978f68f736a749d4e4@sentry.io/1837405',
-  integrations: [new Integrations.Vue({ Vue, attachProps: true })]
-})
+// import Vue from 'vue'
+// import * as Sentry from '@sentry/browser'
+// import * as Integrations from '@sentry/integrations'
+// // 账号在xiaotian这里 需要联系
+// Sentry.init({
+//   dsn: 'https://f741fd4ca0d74d978f68f736a749d4e4@sentry.io/1837405',
+//   integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+// })
