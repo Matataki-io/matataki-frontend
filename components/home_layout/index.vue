57b86404b9085f55fe428242a03e971da8657a2a
xiaotian <952822399@qq.com>
2019-12-23 19:06:51 +0800

    remove dom

--- components/home_layout/index.vue
+++ components/home_layout/index.vue
@@ -14,12 +14,6 @@
                 {{ item.lab }}
               </router-link>
             </div>
-            <!-- <span
-              v-for="(itme, index) in articleCardData"
-              :key="index"
-              :class="nowMainIndex === index && 'active'"
-              @click="nowMainIndex = index"
-            >{{ itme.title }}</span> -->
             <slot name="sort" />
           </div>
           <slot />
@@ -57,14 +51,7 @@
 <script>
 import throttle from 'lodash/throttle'
 import debounce from 'lodash/debounce'
-// import recommendSlide from '~/components/recommendSlide/index.vue'
-// import articleCard from '@/components/articleCard/index.vue'
-// import articleCardListNew from '@/components/article_card_list_new/index.vue'
-// import tags from '@/components/tags/index.vue'
-// import buttonLoadMore from '@/components/button_load_more/index.vue'
-
 import { recommend, paginationData, getTags } from '@/api/async_data_api.js'
-// import banner from '@/components/banner/index.vue'
 import bannerMatataki from '@/components/banner/banner_matataki.vue'
 import RAList from '@/components/recommend_author_list'
 import swipe from '@/components/swipe/index.vue'
@@ -72,12 +59,6 @@ import swipe from '@/components/swipe/index.vue'
 export default {
   transition: 'page',
   components: {
-    // recommendSlide,
-    // articleCard,
-    // articleCardListNew,
-    // tags,
-    // buttonLoadMore,
-    // banner,
     bannerMatataki,
     RAList,
     swipe
@@ -90,9 +71,6 @@ export default {
   },
   data() {
     return {
-      nowMainIndex: 0,
-      recommendList: [],
-      initData: [],
       head: [
         {
           url: 'article',
@@ -107,100 +85,9 @@ export default {
           lab: '我的关注'
         }
       ],
-      articleCardData: [
-        {
-          title: this.$t('home.articleNavHotTitle'),
-          params: {
-            channel: 1,
-            filter: null,
-            extra: 'short_content'
-          },
-          apiUrl: 'homeScoreRanking',
-          articles: [],
-          isAtuoRequest: false
-        },
-        {
-          title: this.$t('home.articleNavNowTitle'),
-          params: {
-            channel: 1,
-            filter: null,
-            extra: 'short_content'
-          },
-          apiUrl: 'homeTimeRanking',
-          articles: [],
-          isAtuoRequest: true
-        },
-        {
-          title: this.$t('home.articleNavFollowTitle'),
-          params: {
-            channel: 1,
-            filter: null,
-            extra: 'short_content'
-          },
-          apiUrl: 'followedPosts',
-          articles: [],
-          isAtuoRequest: true
-        }
-      ],
-      tagCards: [],
       usersRecommendList: [],
-      usersLoading: false,
-      checkedFilter: ['1', '2', '4']
+      usersLoading: false
     }
-  },
-  computed: {
-    filter() {
-      let result = 0
-      for (const item of this.checkedFilter) {
-        result |= parseInt(item)
-      }
-      return result
-    }
-  },
-  watch: {
-    nowMainIndex(value) {
-      this.articleCardData[value].articles = []
-      this.onCheckedFilterChanged()
-    }
-  },
-  async asyncData({ $axios }) {
-    const initData = Object.create(null)
-    try {
-      // 推荐
-      const res = await recommend($axios, 1)
-      if (res.code === 0) initData.recommend = res.data
-      else {
-        const obj = { src: '', title: '' }
-        for (let i = 0; i < 5; i++) initData.recommend = [obj]
-      }
-
-      // 内容列表
-      const params = {
-        channel: 1,
-        extra: 'short_content'
-      }
-      const resPagination = await paginationData(
-        $axios,
-        'homeScoreRanking',
-        params
-      )
-      if (resPagination.code === 0) { initData.paginationData = resPagination.data.list } else initData.paginationData = []
-
-      // tags
-      const resTag = await getTags($axios, 'post')
-      if (resTag.code === 0) initData.tags = resTag.data
-      else initData.tags = []
-
-      return { initData }
-    } catch (error) {
-      console.log(error)
-      return { initData }
-    }
-  },
-  created() {
-    this.recommendList = this.initData.recommend
-    this.articleCardData[0].articles = this.initData.paginationData
-    this.tagCards = this.initData.tags
   },
   mounted() {
     this.usersrecommend()
@@ -229,42 +116,7 @@ export default {
             this.usersLoading = false
           }, 300)
         })
-    }, 800),
-    // 点击更多按钮返回的数据
-    buttonLoadMore(res) {
-      // console.log(res)
-      if (res.data && res.data.list && res.data.list.length !== 0) {
-        this.articleCardData[res.index].articles = this.articleCardData[
-          res.index
-        ].articles.concat(res.data.list)
-      }
-    },
-    promptComputed(index) {
-      return index === 2 ? this.$t('notFollowContent') : this.$t('notArticle')
-    },
-    handleCheckedFilterChanged(value) {
-      this.onCheckedFilterChanged()
-    },
-    onCheckedFilterChanged: debounce(async function () {
-      // This page drives me crazy!!!
-
-      const currentTab = this.articleCardData[this.nowMainIndex]
-
-      currentTab.params.filter = this.filter
-
-      try {
-        const res = await this.$API.getBackendData(
-          { url: currentTab.apiUrl, params: currentTab.params },
-          false
-        )
-        if (res.code !== 0) console.error(res.message)
-        else if (res.data && res.data.list && res.data.list.length !== 0) {
-          currentTab.articles = res.data.list
-        }
-      } catch (error) {
-        console.error(error)
-      }
-    }, 500)
+    }, 800)
   }
 }
 </script>
