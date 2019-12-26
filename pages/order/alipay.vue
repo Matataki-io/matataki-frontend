f0ad0d84e30a4f2d7cf38e847152fed543f43615
shellteo <shellteo@163.com>
2019-12-26 18:25:09 +0800

    add alipay

---
+++ pages/order/alipay.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>alipay!!!</h1>
+    <a :href="href">{{ href }}</a>
+  </div>
+</template>
+
+<script>
+import request from '@/utils/request'
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      href: ''
+    }
+  },
+  mounted() {
+    this.getAliPay()
+  },
+  methods: {
+    getAliPay() {
+      request.post('/alipay/pagePay', {
+        amount: 0.01,
+        title: '测试'
+      }).then(res => {
+        this.href = res.data
+      })
+    }
+  }
+}
+</script>
+
+<style lang="less">
+@purple: #542de0;
+</style>
