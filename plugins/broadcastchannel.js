754539c997b50b14b007237a726f5d8852744077
韦海晖 <franciswei98@gmail.com>
2020-01-09 16:45:42 +0800

    fix: new onmsg

--- plugins/broadcastchannel.js
+++ plugins/broadcastchannel.js
@@ -4,22 +4,19 @@ import { BroadcastChannel } from 'broadcast-channel'
 const userMsgChannel = new BroadcastChannel('user')
 
 userMsgChannel.onmessage = (ev) => {
-  if (!ev.isTrusted) return console.warn('not trusted event catched, i am not doing anything until it is safe again.', ev)
-  console.log('Trusted message event received')
-  if (typeof ev.data === 'string') {
-    switch (ev.data) {
-      case 'login': {
-        console.info('User login from other page, refreshing now to sync login status.')
-        window.location.reload()
-        break
-      }
-      case 'logout': {
-        console.info('User logout from other page, refreshing now to sync login status.')
-        window.location.reload()
-        break
-      }
-      default: console.log('I don\'t know how to handle this: ', ev)
+  console.info('userMsgChannel get new msg from other tab: ', ev)
+  switch (ev) {
+    case 'login': {
+      console.info('User login from other page, refreshing now to sync login status.')
+      window.location.reload()
+      break
     }
+    case 'logout': {
+      console.info('User logout from other page, refreshing now to sync login status.')
+      window.location.reload()
+      break
+    }
+    default: console.log('I don\'t know how to handle this: ', ev)
   }
 }
 // 后续或许不需要注入 window，这个纯粹为了开发方便
