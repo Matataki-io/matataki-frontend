47985643ea1b7d41fa12edbf39975574e2a2c730
xiaotian <952822399@qq.com>
2020-01-17 14:46:01 +0800

    sentry pc

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -1,8 +1,10 @@
-// import Vue from 'vue'
-// import * as Sentry from '@sentry/browser'
-// import * as Integrations from '@sentry/integrations'
-// // 账号在xiaotian这里 需要联系
-// Sentry.init({
-//   dsn: 'https://f741fd4ca0d74d978f68f736a749d4e4@sentry.io/1837405',
-//   integrations: [new Integrations.Vue({ Vue, attachProps: true })]
-// })
+import Vue from 'vue'
+import * as Sentry from '@sentry/browser'
+import * as Integrations from '@sentry/integrations'
+
+if (process.env.NODE === 'production') {
+  Sentry.init({
+    dsn: 'https://c1b6310e663048cc884651886a24a0d8@sentry.io/1885390',
+    integrations: [new Integrations.Vue({ Vue, attachProps: true })]
+  })
+}
