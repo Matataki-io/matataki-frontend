e75bc3b31d09d5ec52517f31920cc29ce9d1f1ac
韦海晖 <franciswei98@gmail.com>
2020-02-06 18:36:21 +0800

    feat: no more upload to ipfs

--- api/API.js
+++ api/API.js
@@ -191,7 +191,7 @@ export default {
    */
   _sendArticle(
     url,
-    { signId = null, author, hash, title, fissionFactor, cover, isOriginal, tags, commentPayPoint, shortContent, cc_license = null },
+    { signId = null, author, data, title, fissionFactor, cover, isOriginal, tags, commentPayPoint, shortContent, cc_license = null },
     signature = null
   ) {
     // 账号类型
@@ -203,7 +203,7 @@ export default {
         author,
         cover,
         fissionFactor,
-        hash,
+        data,
         platform: idProvider,
         publickey: signature ? signature.publicKey : null,
         sign: signature ? signature.signature : null,
@@ -215,7 +215,8 @@ export default {
         cc_license,
         commentPayPoint,
         shortContent
-      }
+      },
+      timeout: 30000
     })
   },
   /**
