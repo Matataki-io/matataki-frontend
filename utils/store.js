47952d1d5965a4522bb22d2b36ed604434c4cfb4
xiaotian <952822399@qq.com>
2020-02-12 12:08:59 +0800

    增加注释

--- utils/store.js
+++ utils/store.js
@@ -16,6 +16,7 @@ export default {
   clear() {
     // 清除全部缓存时不会清空白名单内容
     // 白名单
+    // likeVisible shareVisible userVisible 文章详情的新手引导
     const whitelist = ['likeVisible', 'shareVisible', 'userVisible']
     store.each((value, key) => !whitelist.includes(key) && store.remove(key))
   }
