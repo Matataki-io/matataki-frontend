05306899ff75e4753a2b096d27265c52d3702f8f
xiaotian <952822399@qq.com>
2020-02-19 15:14:27 +0800

    tag page调整

--- pages/_lang/tag/_id.vue
+++ pages/_lang/tag/_id.vue
@@ -14,7 +14,7 @@
             <no-content-prompt :list="item.articles">
               <el-col
                 v-for="(itemChild, indexChild) in item.articles"
-                :key="indexChild"
+                :key="indexChild + itemChild.id"
                 :span="mainNavTypeIndex === 0 ? 24 : 8"
               >
                 <articleCard
@@ -23,7 +23,7 @@
                   :type-index="1"
                   :card-type="'commodity-card tag'"
                 />
-                <articleCardList v-else :card="itemChild" />
+                <articleCardListNew v-else :card="itemChild" />
               </el-col>
             </no-content-prompt>
           </el-row>
@@ -38,7 +38,7 @@
       <!-- 空div控制内容 end -->
 
       <el-col :span="mainNavTypeIndex === 0 ? 8 : 6" class="tags article">
-        <div class="position-sticky top80">
+        <div class="tags-right">
           <span>{{ $t('tag.moreTag') }}</span>
           <tags :type-index="mainNavTypeIndex" :tag-cards="tagCards" class="tags-container" />
         </div>
@@ -51,13 +51,15 @@
 import buttonLoadMore from '@/components/button_load_more/index.vue'
 import tags from '@/components/tags/index.vue'
 import articleCard from '@/components/articleCard/index.vue'
-import articleCardList from '@/components/article_card_list/index.vue'
+// import articleCardList from '@/components/article_card_list/index.vue'
+import articleCardListNew from '@/components/article_card_list_new/index.vue'
 
 export default {
   name: 'Tag',
   components: {
     articleCard,
-    articleCardList,
+    // articleCardList,
+    articleCardListNew,
     tags,
     buttonLoadMore
   },
@@ -143,5 +145,147 @@ export default {
   margin-top: 40px !important;
 }
 
+// home页面内容区域 适用于home commodity tag 页面
+.container {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 60px;
+  .spanStyle{
+    font-size: 20px;
+    color:rgba(0,0,0,1);
+    line-height:1;
+    cursor: pointer;
+    transition: all .3s;
+  }
+  .main {
+    display: flex;
+    flex-direction: column;
+    &.article{
+      // max-width: 787px;
+      // flex: 0 0 787px;
+    }
+    &.commodity{
+      // max-width: 890px;
+      // flex: 0 0 890px;
+      // .commodity-card-content {
+      //   display: flex;
+      //   flex-wrap: wrap;
+      // }
+    }
+  }
+  .tags {
+    &.article{
+      // max-width: 374px;
+      // flex: 0 0 374px;
+    }
+    &.commodity{
+      // max-width: 270px;
+      // flex: 0 0 270px;
+    }
+    &-container {
+      margin-top: 20px;
+      background: #fff;
+      border-radius: @br10;
+      padding: 10px;
+    }
+    span {
+      .spanStyle();
+      font-weight:bold;
+      margin: 0 0 20px;
+    }
+  }
+
+  .main-nav {
+    margin: 0 0 20px;
+    span {
+      .spanStyle();
+      font-weight:400;
+      margin-right: 30px;
+      &.active {
+      font-weight: bold;
+      }
+      em {
+        font-size: 20px;
+        font-style: normal;
+        color: @purpleDark;
+      }
+    }
+
+    .filter {
+      float: right;
+      margin-right: 0px;
+
+      &-button {
+        padding: 0;
+        color: #606266;
+      }
+      &-header {
+        display: flex;
+        align-items: center;
+      }
+      &-icon {
+        width: 22px;
+        height: 22px;
+        margin-right: 4px;
+      }
+    }
+  }
+
+  .recommend-author {
+    padding-bottom: 20px;
+    .ra-head {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .change {
+        width: 20px;
+        height: 20px;
+        //background: @purpleDark;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin-right: 6px;
+        &-icon {
+          width: 72%;
+        }
+      }
+      .ra-head-title {
+        .spanStyle();
+        font-weight: bold;
+      }
+      .ra-head-random {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        font-size:16px;
+        font-weight:bold;
+        color:@purpleDark;;
+        cursor: pointer;
+      }
+    }
+    .ra-content {
+      background:rgba(255,255,255,1);
+      border-radius: @br10;
+      padding: 10px 20px;
+      margin-top: 34px;
+    }
+  }
+}
+
+.load-more-button {
+  display: flex;
+  justify-content: center;
+  min-height: 300px;
+}
+
+.fan-entrance {
+  width: 60px;
+  height: 71px;
+}
+
+.tags-right {
+  position: sticky;
+  top: 80px;
+}
 </style>
-<style lang="less" scoped src="../home_container.less"></style>
