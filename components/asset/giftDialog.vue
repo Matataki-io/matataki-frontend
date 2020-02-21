e533b149aa17c7b383f4af9a9b84f89c2a3ddc36
xiaotian <952822399@qq.com>
2020-02-21 12:01:50 +0800

    搜索为空时, 清空列表

--- components/asset/giftDialog.vue
+++ components/asset/giftDialog.vue
@@ -219,10 +219,14 @@ export default {
         }
       })
     },
+    // 搜索用户
     searchUser: debounce(function () {
       const searchName = this.form.username.trim()
 
-      if (!searchName) return
+      if (!searchName) {
+        this.searchUserList = []
+        return
+      }
 
       this.toUserInfo = null
 
