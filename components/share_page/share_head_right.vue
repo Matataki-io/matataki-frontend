4031ddce140f4d8ad31b55ba0f5f6caa854ee5d7
xiaotian <952822399@qq.com>
2020-02-25 17:50:59 +0800

    分享转让和删除

--- components/share_page/share_head_right.vue
+++ components/share_page/share_head_right.vue
@@ -56,7 +56,7 @@ export default {
           const res = await this.$backendAPI.delArticle({ id })
           if (res.status === 200 && res.data.code === 0) {
             this.$message({ duration: 2000, message: '删除成功' })
-            this.$router.push('/article')
+            this.$router.push('/sharehall')
           }
           else this.$message.error(res.data.message)
         } catch (error) {
