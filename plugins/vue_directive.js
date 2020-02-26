0aed2d39391707b9256772532d51bf081f97aeb5
xiaotian <952822399@qq.com>
2020-02-26 16:16:50 +0800

    修改v-highlight directive

--- plugins/vue_directive.js
+++ plugins/vue_directive.js
@@ -2,15 +2,32 @@ import Vue from 'vue'
 
 // v-highlight 因为自带的这个大部分无法正常工作, 手动set一次
 Vue.directive('highlight',function (el) {
-  let len = 0
-  try {
-    let blocks = el.querySelectorAll('pre code')
-    blocks.forEach((block)=>{
-        hljs.highlightBlock(block)
-    })
-  } catch (error) {
-    len++
-    if (len >= 10) console.log(error)
-    return
+  const setHljs = () => {
+    try {
+      let blocks = el.querySelectorAll('pre code')
+      blocks.forEach((block)=>{
+          hljs.highlightBlock(block)
+      })
+    } catch (error) {
+      console.log(error)
+      return
+    }
   }
+  // 次数
+  let len = 0
+  let timer = null
+
+  timer = setInterval(() => {
+    try {
+      if (hljs || len >= 100) {
+        setHljs()
+        clearInterval(timer)
+      }
+      len++
+    } catch (error) {
+      return
+    }
+  }, 300)
+
+
 })
