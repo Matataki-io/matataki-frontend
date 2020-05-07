2b20c08c9f2c9ecef0371c4532b78ee259ca9e32
xiaotian <952822399@qq.com>
2020-05-07 18:19:46 +0800

    header 响应式

--- plugins/components.js
+++ plugins/components.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import ipfs from '@/common/components/ipfs/index'
 import dialog from '@/common/components/dialog/index'
+import avatar from '@/common/components/avatar'
 
 Vue.component('m-ipfs', ipfs) // ipfs
 Vue.component('m-dialog', dialog) // dialog
+
+Vue.component('c-avatar', avatar) // avatar
