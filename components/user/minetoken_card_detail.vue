c6c6a030e224cdae258dca24c6de3d3d0342d8de
xiaotian <952822399@qq.com>
2020-06-23 11:20:30 +0800

    当指向Fan票流水中的文字名称时会弹出快捷菜单

--- components/user/minetoken_card_detail.vue
+++ components/user/minetoken_card_detail.vue
@@ -2,24 +2,30 @@
   <div class="card">
     <div class="fl jsb">
       <div class="from-to">
-        <router-link
-          :to="{ name: 'user-id', params: { id: card.from_uid } }"
-          class="username"
-          :class="!card.from_username && 'logout'"
-        >
-          {{ from_nickname }}
-        </router-link>
+        <c-user-popover :user-id="Number(card.from_uid)">
+          <router-link
+            :to="{ name: 'user-id', params: { id: card.from_uid } }"
+            class="username"
+            :class="!card.from_username && 'logout'"
+            target="_blank"
+          >
+            {{ from_nickname }}
+          </router-link>
+        </c-user-popover>
         <svg-icon
           icon-class="transfer"
           class="info-icon"
         />
-        <router-link
-          :to="{ name: 'user-id', params: { id: card.to_uid } }"
-          class="username"
-          :class="!card.to_username && 'logout'"
-        >
-          {{ to_nickname }}
-        </router-link>
+        <c-user-popover :user-id="Number(card.to_uid)">
+          <router-link
+            :to="{ name: 'user-id', params: { id: card.to_uid } }"
+            class="username"
+            :class="!card.to_username && 'logout'"
+            target="_blank"
+          >
+            {{ to_nickname }}
+          </router-link>
+        </c-user-popover>
         <txHash
           v-if="card.tx_hash"
           :hash="card.tx_hash"
