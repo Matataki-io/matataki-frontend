063e3040fb39bac0d502385e931d4a4db28c9223
Frank Wei <3261732+frankwei98@users.noreply.github.com>
2020-06-25 17:36:50 +0800

    New translations zh.js (English)

--- locale/en.js
+++ locale/en.js
@@ -137,7 +137,9 @@ export default {
         githubTitle: 'login with GitHub',
         wechatTitle: '微信登录',
         telegramTitle: 'Telegram登录',
-        twitterTitle: 'Twitter登录（开发中）',
+        twitterTitle: 'Twitter登录',
+        facebookTitle: 'Facebook登录',
+        googleTitle: 'Google登录',
         ontDoc: '《如何使用ONT登录》',
         invite: '已邀请',
         //img
