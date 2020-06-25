e78700d9f554419043f0fc1db37b64fda8561302
神樹桜乃 <kodama@sakuno.moe>
2020-06-25 18:01:48 +0800

    Add support of twitter login

---
+++ pages/login/twitter/index.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tg-container">
+    <!-- <div v-loading='loading' style='height: 100%;' element-loading-text='登录中...' /> -->
+    <p class="tips">
+      loading...
+    </p>
+    <p class="tips">
+      使用该功能需要“科学上网”
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  layout: 'empty',
+  data() {
+    return {
+      token: {}
+    }
+  },
+  async mounted() {
+      const response = await this.$API.twitterLoginPrepare()
+
+      window.location.href = 'https://api.twitter.com/oauth/authenticate?oauth_token=' + response.data
+  },
+  methods: {
+  }
+}
+</script>
+
+<style scoped lang='less'>
+.tg-container {
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  align-items: center;
+  padding-top: 10%;
+  .tips {
+    font-size: 14px;
+    color: #333;
+    padding: 0;
+    margin: 2em 0 0;
+  }
+}
+</style>
