aefb41a2ae15dee6bcbad2b65bf6d6702f1b1634
shellteo <shellteo@163.com>
2020-08-03 17:28:11 +0800

    update circle page layout

--- pages/token/_id/circle.vue
+++ pages/token/_id/circle.vue
@@ -10,6 +10,10 @@
       :balance="balance"
       @display-angle="setDisplayAngle"
     />
+    <tokenBuyCard2 
+      :token="minetokenToken"
+      :current-pool-size="currentPoolSize"
+    />
     <tokenNav :display-angle="displayAngle" />
     <el-row class="token-container">
       <!-- 左侧卡片 -->
@@ -19,10 +23,10 @@
       </el-col>
       <!-- 右侧卡片 -->
       <el-col :span="7">
-        <tokenBuyCard
+        <!-- <tokenBuyCard
           :token="minetokenToken"
           :current-pool-size="currentPoolSize"
-        />
+        /> -->
         <tokenJoinFandom
           :token-symbol="minetokenToken.symbol || ''"
           :token-id="Number($route.params.id)"
@@ -49,7 +53,8 @@ import tokenNav from '@/components/token/token_nav'
 // import relatedActivities from '@/components/token/related_activities'
 import tokenRelated from '@/components/token/token_related'
 // 右侧
-import tokenBuyCard from '@/components/token/token_buy_card'
+// import tokenBuyCard from '@/components/token/token_buy_card'
+import tokenBuyCard2 from '@/components/token/token_buy_card2'
 import tokenJoinFandom from '@/components/token/token_join_fandom'
 import relatedWebsites from '@/components/token/related_websites'
 import socialAccount from '@/components/token/social_account'
@@ -63,7 +68,8 @@ export default {
     // relatedActivities,
     tokenRelated,
     // 右侧
-    tokenBuyCard,
+    // tokenBuyCard,
+    tokenBuyCard2,
     tokenJoinFandom,
     relatedWebsites,
     socialAccount,
