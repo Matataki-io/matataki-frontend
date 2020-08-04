0723498f83f98880723817fcb07d5654ca2eb0be
shellteo <shellteo@163.com>
2020-08-04 15:38:20 +0800

    update order type modal

--- components/ExsModal.vue
+++ components/ExsModal.vue
@@ -8,7 +8,12 @@
     custom-class="my-dialog br10 auth-dialog"
   >
     <section class="exs-main">
-      <div disabled class="aui-flex b-line" style="padding:15px 0">
+      <div
+        :disabled="noUniswap"
+        class="aui-flex b-line"
+        style="padding:15px 0" 
+        @click="radio = '1'"
+      >
         <div class="aui-flex-wx">
           <img :src="uniswapLogo" alt="uniswap">
         </div>
@@ -17,12 +22,17 @@
           <p>价格：{{ uniswap.price }} (CNY)</p>
         </div>
         <div class="aui-payment-method">
-          <el-radio v-model="radio" label="1" disabled="">
+          <el-radio v-model="radio" label="1" :disabled="noUniswap">
             &nbsp;
           </el-radio>
         </div>
       </div>
-      <div class="aui-flex b-line" style="padding:15px 0" @click="radio = '2'">
+      <div
+        :disabled="noMarket" 
+        class="aui-flex b-line"
+        style="padding:15px 0"
+        @click="radio = '2'"
+      >
         <div class="aui-flex-wx">
           <img :src="tradeLogo" alt="uniswap">
         </div>
@@ -31,7 +41,7 @@
           <p>价格：{{ directTrade.price }} (CNY)</p>
         </div>
         <div class="aui-payment-method">
-          <el-radio v-model="radio" label="2">
+          <el-radio v-model="radio" label="2" :disabled="noMarket">
             &nbsp;
           </el-radio>
         </div>
@@ -62,6 +72,16 @@ export default {
         signId: ''
       })
     },
+    amount: {
+      type: [String, Number],
+      default: 0
+    },
+    token: {
+      type: Object,
+      default: () => ({
+        id: '',
+      })
+    },
   },
   data() {
     return {
@@ -75,7 +95,10 @@ export default {
       directTrade: {
         balance: 0,
         price: 0
-      }
+      },
+      noUniswap: true,
+      noMarket: true,
+      loading: true
     }
   },
   computed: {
@@ -84,6 +107,9 @@ export default {
     },
     uniswapLogo() {
       return this.$API.getImg('/avatar/exchange.png')
+    },
+    tokenId() {
+      return this.token.id
     }
   },
   watch: {
@@ -95,17 +121,19 @@ export default {
     }
   },
   mounted() {
-    this.getPoolSize(29)
+    if (this.tokenId) this.getExsInfo(this.tokenId)
   },
   methods: {
     async createOrder() {
       let params = {
-        ...this.form,
+        output: this.amount,
+        outputToken: this.token
       }
       if (this.radio === '2') params.type = 'direct_trade'
       this.$store.dispatch('order/createOrder', params)
     },
-    async getPoolSize(tokenId) {
+    async getExsInfo(tokenId) {
+      this.loading = true
       const uniswapResult = await this.$API.getCurrentPoolSize(tokenId)
       // const price = await this.$API.getOutputAmount(tokenId, 0, 10000)
       let uniswapBalance = 0
@@ -115,18 +143,27 @@ export default {
         uniswapBalance = token_amount
         uniswapPrice = token_amount === 0 ? 0 : cny_amount / token_amount
         if (uniswapPrice < 1) uniswapPrice = 1
+        this.noUniswap = false
+      } else {
+        this.noUniswap = true
       }
+      if (uniswapBalance <= 0) this.noUniswap = true
       let directTradeBalance = 0
       let directTradePrice = 0
       const marketResult = await this.$API.directTrade.getItem(tokenId)
       if (marketResult.code === 0) {
         directTradeBalance = marketResult.data.balance
         directTradePrice = marketResult.data.price
+        this.noMarket = false
+      } else {
+        this.noMarket = true
       }
+      if (directTradeBalance <= 0) this.noMarket = true
       this.uniswap.balance =  this.$utils.fromDecimal(uniswapBalance)
       this.uniswap.price =  this.$utils.fromDecimal(uniswapPrice)
       this.directTrade.balance =  this.$utils.fromDecimal(directTradeBalance)
       this.directTrade.price =  this.$utils.fromDecimal(directTradePrice)
+      this.loading = false
     }
   }
 }
