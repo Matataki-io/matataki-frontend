bb17e5da9a4392bcabac9b23a2fd2b204a7e80cd
LittleSound <464388324@qq.com>
2020-11-03 19:51:17 +0800

    添加了 twitter 卡片的文本处理组件

---
+++ components/twitter_card/twitter_content.vue
@@ -0,0 +1,63 @@
+<template>
+  <p class="twitter-content" v-html="content" />
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+  props: {
+    // 卡片数据
+    card: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    content () {
+      let text = this.removeTwitterLink(this.card.text)
+      text = this.annotateHighlightText(text)
+      return text
+    }
+  },
+  methods: {
+    removeTwitterLink(text) {
+      return text.replace(/https:\/\/t.co\/.+$/gm, '')
+    },
+    annotateHighlightText(text) {
+      const textArr = text.split('')
+      if (this.card.entities) {
+        const { user_mentions, hashtags } = this.card.entities
+        if (user_mentions) {
+          user_mentions.forEach(mention => {
+            textArr[mention.indices[0]] = '<span>' + textArr[mention.indices[0]]
+            textArr[mention.indices[1] - 1] = textArr[mention.indices[1] - 1] + '</span>'
+          })
+        }
+
+        if (hashtags) {
+          hashtags.forEach(hashtag => {
+            textArr[hashtag.indices[0]] = '<span>' + textArr[hashtag.indices[0]]
+            textArr[hashtag.indices[1] - 1] = textArr[hashtag.indices[1] - 1] + '</span>'
+          })
+        }
+      }
+      return textArr.join('')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+.twitter-content {
+  color: black;
+  white-space: pre-line;
+  /deep/ span {
+    color: #1b95e0;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
