73373941406e731f3b05f5c96ccd2507f462e7e9
LittleSound <464388324@qq.com>
2020-11-03 19:43:13 +0800

    对接了 twitter 卡片的图片数据，并支持懒加载和点击预览功能。

--- components/twitter_card/twitter_photo_album.vue
+++ components/twitter_card/twitter_photo_album.vue
@@ -2,28 +2,64 @@
   <div class="album">
     <!-- 单张图片 -->
     <div v-if="imgUrls.length === 1" class="album-one">
-      <img :src="imgUrls[0]" alt="image">
+      <el-image
+        :src="imgUrls[0]"
+        alt="image"
+        :preview-src-list="imgUrls"
+        fit="cover"
+        lazy
+      />
     </div>
     <!-- 两张图片 -->
     <div v-else-if="imgUrls.length === 2" class="album-two">
       <div class="album-two-column">
-        <img :src="imgUrls[0]" alt="image">
+        <el-image
+          :src="imgUrls[0]"
+          alt="image"
+          :preview-src-list="imgUrls"
+          fit="cover"
+          lazy
+        />
       </div>
       <div class="album-two-column">
-        <img :src="imgUrls[1]" alt="image">
+        <el-image
+          :src="imgUrls[1]"
+          alt="image"
+          :preview-src-list="imgUrls"
+          fit="cover"
+          lazy
+        />
       </div>
     </div>
     <!-- 三张图片 -->
     <div v-else-if="imgUrls.length === 3" class="album-three">
       <div class="album-three-column">
-        <img :src="imgUrls[0]" alt="image">
+        <el-image
+          :src="imgUrls[0]"
+          alt="image"
+          :preview-src-list="imgUrls"
+          fit="cover"
+          lazy
+        />
       </div>
       <div class="album-three-column">
         <div class="album-three-column-line">
-          <img :src="imgUrls[2]" alt="image">
+          <el-image
+            :src="imgUrls[1]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
         <div class="album-three-column-line">
-          <img :src="imgUrls[2]" alt="image">
+          <el-image
+            :src="imgUrls[2]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
       </div>
     </div>
@@ -31,18 +67,42 @@
     <div v-else-if="imgUrls.length > 3" class="album-three">
       <div class="album-three-column">
         <div class="album-three-column-line">
-          <img :src="imgUrls[0]" alt="image">
+          <el-image
+            :src="imgUrls[0]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
         <div class="album-three-column-line">
-          <img :src="imgUrls[2]" alt="image">
+          <el-image
+            :src="imgUrls[2]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
       </div>
       <div class="album-three-column">
         <div class="album-three-column-line">
-          <img :src="imgUrls[1]" alt="image">
+          <el-image
+            :src="imgUrls[1]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
         <div class="album-three-column-line">
-          <img :src="imgUrls[3]" alt="image">
+          <el-image
+            :src="imgUrls[3]"
+            alt="image"
+            :preview-src-list="imgUrls"
+            fit="cover"
+            lazy
+          />
         </div>
       </div>
     </div>
@@ -54,7 +114,7 @@
 export default {
   props: {
     // 卡片数据
-    imgs: {
+    media: {
       type: Array,
       required: true
     },
@@ -65,8 +125,7 @@ export default {
   },
   computed: {
     imgUrls () {
-      const img = 'https://picsum.photos/1280/720?random'
-      return [ ...Array(this.length) ].map(() => img)
+      return this.media
     }
   }
 }
@@ -90,6 +149,10 @@ img {
     overflow: hidden;
     width: 100%;
     height: 100%;
+    .el-image {
+      width: 100%;
+      height: 100%;
+    }
   }
 
   &-two {
@@ -97,6 +160,10 @@ img {
     width: 100%;
     height: 100%;
     display: flex;
+    .el-image {
+      width: 100%;
+      height: 100%;
+    }
     &-column {
       height: 100%;
       flex: 1;
@@ -113,13 +180,17 @@ img {
     width: 100%;
     height: 100%;
     display: flex;
+    .el-image {
+      width: 100%;
+      height: 100%;
+    }
     &-column {
       height: 100%;
       flex: 1;
       display: flex;
       flex-direction: column;
       &-line {
-        height: 100%;
+        height: 50%;
         flex: 1;
 
         &:nth-child(1) {
