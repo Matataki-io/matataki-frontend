8322cc1e70f17178932c978c9ef3b44db253ab12
LittleSound <464388324@qq.com>
2020-11-03 20:02:19 +0800

    完善了 显示更多回复 组件的 UI

--- components/twitter_card/more_replies.vue
+++ components/twitter_card/more_replies.vue
@@ -1,8 +1,13 @@
 <template>
   <div class="cardunit-bg">
-    <div class="cardunit-bg-uline" />
-    <div class="cardunit-bg-uline" />
-    <div class="cardunit-bg-uline" />
+    <div class="cardunit-bg-omit">
+      <div class="cardunit-bg-omit-point" />
+      <div class="cardunit-bg-omit-point" />
+      <div class="cardunit-bg-omit-point" />
+    </div>
+    <span class="cardunit-bg-more">
+      显示更多回复
+    </span>
   </div>
 </template>
 
@@ -15,19 +20,33 @@ export default {
 <style lang="less" scoped>
 .cardunit-bg {
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  justify-content: space-between;
   height: 39px;
-  padding: 12px 0;
   box-sizing: border-box;
+  align-items: center;
 
-  .cardunit-bg-uline {
-    display: block;
-    height: 2px;
-    width: 2px;
-    margin-left: 23.5px;
-    background: #ccd6dd;
+  &-omit {
+    height: 39px;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: space-between;
+    padding: 12px 0 12px;
+    margin-right: 10px;
+    &-point {
+      display: block;
+      height: 2px;
+      width: 2px;
+      margin-left: 23.5px;
+      margin-right: 23.5px;
+      background: #ccd6dd;
+    }
+  }
+  &-more {
+    color: #1b95e0;
+    font-size: 15px;
+    font-weight: 400;
+    line-height: 20px;
   }
 }
 </style>
