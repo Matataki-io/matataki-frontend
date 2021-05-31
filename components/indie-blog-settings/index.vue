37fd6a9722e78c705f698741b2bc39dd099f5510
Garfield Lee <openpgp@550.moe>
2021-05-31 15:06:51 +0800

    fix: try to fix chinese language when theme is cake

--- components/indie-blog-settings/index.vue
+++ components/indie-blog-settings/index.vue
@@ -252,7 +252,7 @@ export default Vue.extend({
       ],
       settings: {},
       oldSettings: {},
-      languages: ['en', 'zh', 'zh-TW', 'zh-HK', 'ja', 'fr', 'es'],
+      languages: ['en', 'zh-CN', 'zh-TW', 'zh-HK', 'ja', 'fr', 'es'],
       themeList: [],
       saving: false,
       loadingPagesStatus: false,
@@ -356,7 +356,12 @@ export default Vue.extend({
     /** 保存设置 POST /user/siteConfig */
     saveSettings() {
       this.saving = true
-      this.$API.changeIndieBlogSiteConfig(this.settings).then((res) => {
+      // Fix issue #1257 https://github.com/Matataki-io/Matataki-FE/issues/1257
+      const siteSettings = this.settings
+      if (siteSettings.theme === 'cake') {
+        siteSettings.language = 'zh'
+      }
+      this.$API.changeIndieBlogSiteConfig(siteSettings).then((res) => {
         this.saving = false
         if (!res) {
           this.$message.error('保存设置失败')
